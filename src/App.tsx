4839b61ea8d2bceee3474ed9699b34bac7ff9b8f
lorena-mattos <[lorena.mattos6@gmail.com]>
2022-07-04 22:56:46 -0300

    criação do desafio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css';
+
+import './styles/app.css';
+
+function App() {
+  return (
+    <div>
+      <h1>teste</h1>
+      <p className='exemplo1'>Mostrando como estilizar com classe</p>
+      <span id='exemplo2'>Exemplo estilzar com id</span>
+    </div>
+  )
+}
+
+export default App
